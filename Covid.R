6e5bedfa98ae5d2cc8d74f2bc590e6ffa2fdde01
Gustavo-Sousa <gustavomotax2@gmail.com>
2020-03-31 16:50:14 -0300

    Deleting files and code refactoring

--- Covid.R
+++ Covid.R
@@ -18,13 +18,13 @@ plot(1:5388,covid$Cases)
 dates <- unique(covid$DateRep[order(covid$DateRep)])
 number_days <- nrow(dates)
 
-# Numero de casos por dia no mundo
-casos.dia <- tapply(covid$Cases,covid[,"DateRep"],sum)
-plot(casos.dia,lty=1,type='l')
+# Number of cases per day
+cases.day <- tapply(covid$Cases,covid[,"DateRep"],sum)
+plot(cases.day,lty=1,type='l')
 
-summary(casos.dia) # sumarios estatisticos
-hist(casos.dia)
-boxplot(casos.dia)
+summary(cases.day) # sumarios estatisticos
+hist(cases.day)
+boxplot(cases.day)
 
 countries_list <- unique(covid$`Countries and territories`)
 
@@ -40,11 +40,10 @@ n_countries_infected <- nrow(countries_infected)
 # Countries without infections
 countries_winfections <- countr_cases[countr_cases == 0]
 
-# Ordered index of cases per country
-ind_OrdCounCas <- order(countr_cases, decreasing = TRUE)
+ind.countrcases <- order(countr_cases, decreasing = TRUE) # Ordered index of cases per country
 
 #The 10 countries with most cases
-top_10Most <- countr_cases[ind_OrdCounCas[1:10]]
+top_10Most <- countr_cases[ind.countrcases[1:10]]
 barplot(top_10Most)
 
 #Number of infected countries per day
@@ -54,21 +53,12 @@ plot(infected_countries_day)
 # Countries with infections
 infected_countries <- tapply(covid$`Countries and territories`, covid$Cases > 0, sum)
 
-df.final <- data.frame(date=c(), country=c())
-for(date in dates){
-    ppp <-  covid %>%
-            filter(Cases > 0 & DateRep == date) %>%
-            select(`Countries and territories`)
-    df.final <- rbind(df.final, date,nrow(ppp))
-}
-
-#df.infcday <- data.frame(date=c(),number=c())
 #Cases per months
 case_month <- tapply(covid$Cases, covid$Month, sum)
 
 # Amostra
 # Day of the first infection on countries
-df.final2 <- data.frame(datee=character(0), country=character(0))
+first_infection <- data.frame(datee=character(0), country=character(0))
 for(country in unique(covid$`Countries and territories`)){
   ppp <-  covid %>%
           filter(`Countries and territories` == country)
@@ -77,39 +67,38 @@ for(country in unique(covid$`Countries and territories`)){
   Dattes <- ppp$DateRep[indde]
   #class(Dattes[ind2])
   newDF <- data.frame(Dattes[ind2],country)
-  names(newDF) <- names(df.final2)
-  df.final2 <- rbind(df.final2, newDF)
+  names(newDF) <- names(first_infection)
+  first_infection <- rbind(first_infection, newDF)
 }
 # Its wrong
-first_infection <- df.final2
 
 # Day of the first one death on countries
 # Only the countries where the deaths starts with one victim
-df.final3 <- data.frame(datee=character(0), country=character(0))
+first.deaths <- data.frame(datee=character(0), country=character(0))
 for(country in unique(covid$`Countries and territories`)){
   ppp <-  covid %>%
           filter(`Countries and territories` == country)
   indde <- order(ppp$DateRep, decreasing = FALSE)
   ind2 <- match(1, ppp$Deaths[indde])
   Dattes <- ppp$DateRep[indde]
-  #class(Dattes[ind2])
+  
   newDF <- data.frame(Dattes[ind2],country)
-  names(newDF) <- names(df.final3)
-  df.final3 <- rbind(df.final3, newDF)
+  names(newDF) <- names(first.deaths)
+  first.deaths <- rbind(first.deaths, newDF)
 }
-
-#df.final3$country[!is.na(df.final3$datee)]
 # Its wrong
-first_deaths <- df.final3 %>%
+
+#first.deaths$country[!is.na(first.deaths$datee)]
+first.deaths <- first.deaths %>%
                 filter(!is.na(datee)) %>%
                 select(datee, country)
 
 # Number of deaths per country
-mortes.pais <- tapply(covid$Deaths,covid[,8],sum)
-pie(mortes.pais[mortes.pais > 50])
+deaths.country <- tapply(covid$Deaths,covid[,8],sum)
+pie(deaths.country[deaths.country > 50])
 
 # Only countries with deaths and their numbers
-pais_mortes <- mortes.pais[which(mortes.pais > 0)]
+pais_mortes <- deaths.country[which(deaths.country > 0)]
 
 # Number of countries with deaths
 n_pais_mortes <- nrow(pais_mortes)
@@ -124,21 +113,21 @@ plot(deaths.country)
 wd_countries <- tapply(covid$Deaths == 0,covid$DateRep, sum) #Acho que ta errado
 
 # Paises sem mortes
-pais_sem_mortes <- mortes.pais[which(mortes.pais == 0)]
+pais_sem_mortes <- deaths.country[which(deaths.country == 0)]
 n_pais_sem_mortes <- nrow(pais_sem_mortes)
 
 # Quantos pa?ses em m?dia possuem menos de 10 infectados
 # M?dia de pa?ses com menos de 10 infectados
 
 # Mortos por infectado e por dia
-tax_mort <-  data.frame(PerInfect=(mortes.pais*100)/countr_cases, perday=mortes.pais/78)
+tax_mort <-  data.frame(PerInfect=(deaths.country*100)/countr_cases, perday=deaths.country/78)
 
 # General mortality
 tax_genDeath <- sum(covid$Deaths)*100/sum(covid$Cases)
 
 # Quantos foram infectados em m?dia por dia
 # Average infections recorded per day
-avg_infections_day <- mean(casos.dia)
+avg_infections_day <- mean(cases.day)
 
 # Quantos morreram em m?dia por dia
 # Averages deaths recorded per day
@@ -159,7 +148,7 @@ avg_countries_infected <- n_countries_infected/145
 avg_countries_winfections <- 1 - avg_countries_infected
 
 # Average deaths per country
-avg_deaths_countries <- mean(mortes.pais)
+avg_deaths_countries <- mean(deaths.country)
 
 # Average infections per country
 avg_infections_country <- mean(countr_cases)
@@ -167,11 +156,11 @@ avg_infections_country <- mean(countr_cases)
 # Qual varia mais: mortes ou infec??es por pa?s?
 # Coeficiente de varia??o infec??es e mortes por pa?s
 cv_cases_country <- sd(countr_cases)/mean(countr_cases)
-cv_deaths_country <- sd(mortes.pais)/mean(mortes.pais) # This
+cv_deaths_country <- sd(deaths.country)/mean(deaths.country) # This
 
 # Do deaths or infections vary more per day ?
 cv_deaths_day <- sd(deaths.day)/avg_deaths_day # This
-cv_cases_day <- sd(casos.dia)/avg_infections_day
+cv_cases_day <- sd(cases.day)/avg_infections_day
 
 # Quantos dias em m?dia da primeira infe??o at? a primeira morte
 # M?dia do intervalo de dias desde infec??o at? a morte
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Covid-19 Statistics Exercise
+
+This is an exercise about Covid-19 spread using R. Course of Introduction to Probability and Statistics.
+
+## Observations
+
+It is my first use of the language.
+
